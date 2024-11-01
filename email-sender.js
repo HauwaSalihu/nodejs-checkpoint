b90823c352147431d4f02a7eb04737f2328ead77
HauwaSalihu <hauwasalih1010@gmail.com>
2024-11-01 15:50:04 +0100

    first commit

--- email-sender.js
+++ email-sender.js
@@ -1,24 +1,26 @@
-import nodemailer from "nodemailer"
+import nodemailer from 'nodemailer';
 
-var transporter = nodemailer.createTransport({
+// Create a transporter
+const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: 'aberdeencommercial@gmail.com',
-    pass: ''
+    pass: 'vntl ewsj qbib yfgk'
   }
 });
 
-var mailOptions = {
+// Define the mail options
+const mailOptions = {
   from: 'aberdeencommercial@gmail.com',
   to: 'hauwasalih1010@gmail.com',
-  subject: 'Sending Email using Node.js',
-  text: 'That was easy!'
+  subject: 'Test Email from Node.js',
+  text: 'Hello, this is a test email sent using nodemailer!'
 };
 
-transporter.sendMail(mailOptions, function(error, info){
+// Send the email
+transporter.sendMail(mailOptions, (error, info) => {
   if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
+    return console.log(`Error: ${error}`);
   }
+  console.log(`Email sent: ${info.response}`);
 });
